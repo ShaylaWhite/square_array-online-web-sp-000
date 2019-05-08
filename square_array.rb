d765147f0c4fbfa36cf3820105bbf640591d4261
Shayla White <shayladwhite1@gmail.com>
2019-05-08 15:55:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@
 counter = 1
 def square_array(array)
  numbers = [1,2,3]
- numbers.each do |Beyonce|
-   puts "The Queen is #{Beyonce}!"
+ numbers.each do |numbers|
+   puts "The Queen is #{numbers}!"
    counter += 1
 end
