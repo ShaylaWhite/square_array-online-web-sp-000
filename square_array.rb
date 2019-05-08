15ecc30d281b5c97c9990ebf9755a82b061ddaa2
Shayla White <shayladwhite1@gmail.com>
2019-05-08 16:07:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,6 @@
-numbers = [1, 4, 9]
+numbers = [1, 2, 9]
 
 def square_array(array)
-counter = 1
-new_numbers = [1, 2, 2]
-numbers.each do |numbers|
-  puts "Stop hitting yourself #{numbers}!"
-   puts "Stop hitting yourself #{counter}!"
-   counter += 1
-   square_array(new_numbers)
+
 end
 end
